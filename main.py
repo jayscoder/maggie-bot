0faca23f84d1b5278f26e88b892c5f460730023e
王童 <785271992@qq.com>
2023-04-12 23:12:01 +0800

    d

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import os
 import discord
-from config import *
+import constantly
+from enum import Flag, auto
+import re
+from discord.ext import commands
+import discord
+from discord_slash import SlashCommand, SlashContext
+slash = SlashCommand(bot, sync_commands=True)
+
+DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
 
 # 定义bot登陆事件
 @bot.event
@@ -11,12 +23,20 @@ async def on_ready():
             # await channel.send('我上线啦')
             print(channel)
 
+
+@bot.command("")
+
 # 定义bot接受到消息的事件
 @bot.event
 async def on_message(message: discord.Message):
+    if message.author == bot.user:
+        return
+    print(message.content)
     await message.channel.send(f"收到: {message.content}")
 
 
-
+@slash.slash(name="hello", description="Says hello to the user")
+async def hello(ctx: SlashContext):
+    await ctx.send(f"Hello {ctx.author.mention}!")
 
 bot.run(DISCORD_BOT_TOKEN)
